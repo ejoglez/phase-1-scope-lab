13056a1ecf4b709312f6e4516a49c281209a153c
Eric Gonzalez <ejoglez@gmail.com>
2022-02-02 11:54:10 -0600

    commit tree e2ed1c1d9417637c6fd56a12228744d549dc7dae

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    var newBestCustomer = "maybe bob";
+
+    bestCustomer = newBestCustomer;
+    return newBestCustomer;
+}
+
+const leastFavoriteCustomer = "Bob";
+
+function changeLeastFavoriteCustomer(){
+    var newLeastFavoriteCustomer = "not bob"
+
+    leastFavoriteCustomer = newLeastFavoriteCustomer;
+    
+    return newLeastFavoriteCustomer;
+}
